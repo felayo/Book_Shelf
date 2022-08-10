e97d315829441c71c6898dd99c6ce6c50e366f91
Ayorinde <ajibaye@gmail.com>
2022-08-10 05:43:49 +0100

    gitignore modified

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const config = require('./config/config').get(process.env.NODE_ENV);
+
 const app = express();
 
 mongoose.Promise = global.Promise;
